bdcb05f7946b8e4630514850165e728be3a4db96
kmaune0 <kmaune71@gmail.com>
2014-06-20 17:20:40 -0700

    added queue file

---
+++ queue.go
@@ -0,0 +1,78 @@
+// queue.go
+// (C) 2014 splashinn
+
+package queue
+
+import (
+  "fmt"
+  "math/rand"
+  "os"
+  "time"
+)
+
+const (
+    QUEUE_SIZE = 5;
+    MAX_INT = 100;
+)
+
+const (
+    QUEUE_SIZE = 5;
+    MAX_INT = 100;
+)
+
+type Queue struct {
+    items []interface{}
+}
+
+func (q *Queue) size() int {
+  return len(q.items)
+}
+
+func (q *Queue) enqueue(item interface{}) {
+  q.items = append(q.items, item)
+}
+
+// removes the least recent item that was added
+func (q *Queue) dequeue() interface{} {
+  if (q.isEmpty()) {
+    fmt.Println("queue has no items to dequeue!")
+    os.Exit(1)
+  }
+  removal := q.items[0]
+  q.items = q.items[1:]
+  return &removal
+}
+
+func (q *Queue) isEmpty() bool {
+  if (q.size() == 0) {
+    return true
+  }
+  return false
+}
+
+func (q *Queue) String() string {
+  out := "queue contents:"
+  for i, item := range q.items {
+    out += fmt.Sprintf("\n[%d] : %3v", i, item)
+  }
+  return out
+}
+
+func create() *Queue {
+  q := new(Queue)
+  for i := 0; i < QUEUE_SIZE; i++ {
+    item := rand.Intn(MAX_INT + 1)
+    q.enqueue(item)
+  }
+  return q
+}
+
+func Run() {
+  rand.Seed(time.Now().UnixNano())
+  q := create()
+  fmt.Println(q)
+  fmt.Println("removing items from queue, oldest first")
+  for _, item := range q.items {
+    fmt.Printf("dequeued: %v\n", item)
+  }
+}
